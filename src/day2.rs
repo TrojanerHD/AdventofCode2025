3f8833ab6033c09f1594354bb0e5750620c40943
TrojanerHD <mail@trojaner.dev>
2025-12-02 20:56:15 +0100

    Use more functional programming for day 2

--- src/day2.rs
+++ src/day2.rs
@@ -1,83 +1,56 @@
-struct Range {
-    start: usize,
-    end: usize,
-}
 pub fn part1(input: &str) -> String {
-    let ranges = input
+    input
         .lines()
         .next()
         .unwrap()
         .split(",")
-        .map(|range| {
+        .flat_map(|range| {
             let (start, end) = range.split_once("-").unwrap();
-            Range {
-                start: start.parse().unwrap(),
-                end: end.parse().unwrap(),
-            }
-        })
-        .collect::<Vec<_>>();
-
-    let mut res = 0;
-    for range in ranges {
-        let mut count = range.start;
-        while count <= range.end {
-            let count_string = count.to_string();
-            if count_string.len() % 2 == 0 {
-                let split = count_string.split_at(count_string.len() / 2);
-                if split.0 == split.1 {
-                    res += count;
+            (start.parse::<usize>().unwrap()..=end.parse().unwrap()).map(|count| {
+                let count_string = count.to_string();
+                if count_string.len() % 2 == 0 {
+                    let split = count_string.split_at(count_string.len() / 2);
+                    if split.0 == split.1 {
+                        return count;
+                    }
                 }
-            }
-            count += 1;
-        }
-    }
-
-    res.to_string().to_owned()
+                0
+            })
+        })
+        .sum::<usize>()
+        .to_string()
+        .to_owned()
 }
 
 fn split_at_every(input: &str) -> usize {
-    'outer: for i in 1..=input.len() / 2 {
-        let split = input.split_at(i);
-        let truth = split.0;
-        let mut rest = split.1;
-        while rest.len() >= i {
-            let split_n = rest.split_at(i);
-            if truth != split_n.0 {
-                continue 'outer;
+    (1..=input.len() / 2)
+        .filter(|&i| input.len().is_multiple_of(i))
+        .find_map(|i| {
+            let (truth, mut rest) = input.split_at(i);
+            while !rest.is_empty() {
+                let split_n = rest.split_at(i);
+                if truth != split_n.0 {
+                    return None;
+                }
+                rest = split_n.1;
             }
-            rest = split_n.1;
-        }
-        if rest.is_empty() || rest == truth {
-            return input.parse::<usize>().unwrap();
-        }
-    }
-    0
+            Some(input.parse::<usize>().unwrap())
+        })
+        .unwrap_or(0)
 }
 
 pub fn part2(input: &str) -> String {
-    let ranges = input
+    input
         .lines()
         .next()
         .unwrap()
         .split(",")
-        .map(|range| {
+        .flat_map(|range| {
             let (start, end) = range.split_once("-").unwrap();
-            Range {
-                start: start.parse().unwrap(),
-                end: end.parse().unwrap(),
-            }
+            (start.parse::<usize>().unwrap()..=end.parse().unwrap())
+                .map(|count| split_at_every(&count.to_string()))
         })
-        .collect::<Vec<_>>();
-
-    let mut res = 0;
-    for range in ranges {
-        let mut count = range.start;
-        while count <= range.end {
-            let count_string = count.to_string();
-            res += split_at_every(&count_string);
-            count += 1;
-        }
-    }
-
-    res.to_string().to_owned()
+        .sum::<usize>()
+        .to_string()
+        .to_owned()
 }
