c4602a9cb1f1f89ed5b8919f32e105f6df08e610
TrojanerHD <mail@trojaner.dev>
2025-12-09 09:06:28 +0100

    Optimize code a bit and apply cargo clippy suggestions

--- src/day9.rs
+++ src/day9.rs
@@ -1,6 +1,4 @@
-use std::usize;
-
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq, PartialOrd)]
 struct Point {
     x: usize,
     y: usize,
@@ -38,7 +36,7 @@ pub fn part1(input: &str) -> String {
     let mut max = 0;
     for point1 in points.iter() {
         for point2 in points.iter() {
-            if point2.y <= point1.y && point2.x <= point1.x {
+            if point2 <= point1 {
                 continue;
             }
             let sub = point1.abs_diff(point2);
@@ -69,8 +67,7 @@ pub fn part2(input: &str) -> String {
     let mut green = vec![(0, 0); y_max + 1];
     let mut min = None;
     let mut max = None;
-    for y in 0..=y_max {
-        // println!("y {y}");
+    for (y, val) in green.iter_mut().enumerate() {
         let mut y_points = points
             .iter()
             .enumerate()
@@ -82,8 +79,6 @@ pub fn part2(input: &str) -> String {
             if let Some(min_x) = min_x
                 && let Some(max_x) = max_x
             {
-                min = None;
-                max = None;
                 let mut before_min = wrapping(min_x.0, true, points.len());
                 let mut after_min = wrapping(min_x.0, false, points.len());
                 let mut before_max = wrapping(max_x.0, true, points.len());
@@ -92,10 +87,6 @@ pub fn part2(input: &str) -> String {
                 while points[before_min].y <= y && points[after_min].y <= y {
                     before_min = wrapping(before_min, true, points.len());
                     after_min = wrapping(after_min, false, points.len());
-                    // println!(
-                    //     "before_min {before_min} for {} with {:?}, after_min {after_min} for {} with {:?}",
-                    //     min_x.0, points[before_min], max_x.0, points[after_min]
-                    // );
                 }
 
                 while points[before_max].y <= y && points[after_max].y <= y {
@@ -104,27 +95,19 @@ pub fn part2(input: &str) -> String {
                 }
 
                 if points[before_min].y > y {
-                    // println!("min x: {:?}", points[before_min]);
                     min = Some(points[before_min].x);
-                }
-                if points[after_min].y > y {
-                    // println!("min x: {:?}", points[after_min]);
+                } else {
                     min = Some(points[after_min].x);
                 }
+
                 if points[before_max].y > y {
                     max = Some(points[before_max].x);
-                }
-                if points[after_max].y > y {
+                } else {
                     max = Some(points[after_max].x);
                 }
-                let Some(min) = min else {
-                    panic!("min is None and min_x is {:?} at y {y}", min_x);
-                };
-                let Some(max) = max else {
-                    panic!("max is None");
-                };
-
-                green[y] = (min, max);
+                let min = min.unwrap();
+                let max = max.unwrap();
+                *val = (min, max);
             }
             continue;
         }
@@ -139,11 +122,6 @@ pub fn part2(input: &str) -> String {
             }
         } else {
             min_x = Some(y_points[0]);
-            // if let Some(act_min) = min
-            //     && let Some(found) = y_points.iter().find(|point| point.1.x == act_min)
-            // {
-            //     min_x = Some(*found);
-            // }
         }
         if let Some(act_max_x) = max_x {
             if let Some(act_max) = max
@@ -155,27 +133,19 @@ pub fn part2(input: &str) -> String {
                 max_x = Some(y_points[y_points.len() - 1]);
             }
         } else {
-            // if let Some(act_max) = max
-            //     && let Some(found) = y_points.iter().find(|point| point.1.x == act_max)
-            // {
-            //     max_x = Some(*found);
-            // }
             max_x = Some(y_points[y_points.len() - 1]);
         }
         let act_min_x = min_x.unwrap();
         let act_max_x = max_x.unwrap();
-        for x in act_min_x.1.x..=act_max_x.1.x {
-            green[y] = (act_min_x.1.x, act_max_x.1.x);
-        }
+        *val = (act_min_x.1.x, act_max_x.1.x);
+        min = None;
+        max = None;
     }
 
-    // println!("green {:?}", green);
-
-    let mut max = (0, (&Point { x: 0, y: 0 }, &Point { x: 0, y: 0 }));
+    let mut max = 0;
     for point1 in points.iter() {
-        // println!("{i} / {}", points.len());
         'innerPoint: for point2 in points.iter() {
-            if point2.y <= point1.y && point2.x <= point1.x {
+            if point2 <= point1 {
                 continue;
             }
             let sub = point1.abs_diff(point2);
@@ -184,20 +154,14 @@ pub fn part2(input: &str) -> String {
             let max_x = point1.x.max(point2.x);
             let min_y = point1.y.min(point2.y);
             let max_y = point1.y.max(point2.y);
-            for y in min_y..=max_y {
-                if green[y].0 > min_x || green[y].1 < max_x {
+            for (min, max) in green.iter().take(max_y + 1).skip(min_y) {
+                if *min > min_x || *max < max_x {
                     continue 'innerPoint;
                 }
             }
-            max = if max.0 < area {
-                (area, (point1, point2))
-            } else {
-                max
-            }
+            max = max.max(area);
         }
     }
 
-    // println!("{:?}", max);
-
-    max.0.to_string().to_owned()
+    max.to_string().to_owned()
 }
