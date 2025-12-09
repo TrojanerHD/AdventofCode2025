03864d3271f0c0eec4ad30dda94aa40938574e92
TrojanerHD <mail@trojaner.dev>
2025-12-09 12:43:46 +0100

    Make day 9 robust to edge cases

--- src/day9.rs
+++ src/day9.rs
@@ -49,116 +49,132 @@ pub fn part1(input: &str) -> String {
     max.to_string().to_owned()
 }
 
+#[derive(Debug, PartialEq, PartialOrd, Clone)]
+struct Point2 {
+    point: Point,
+    index: Option<usize>,
+}
+
+impl Point2 {
+    fn new(x: usize, y: usize, index: Option<usize>) -> Self {
+        Self {
+            point: Point { x, y },
+            index,
+        }
+    }
+    fn x(&self) -> usize {
+        self.point.x
+    }
+    fn y(&self) -> usize {
+        self.point.y
+    }
+    fn abs_diff(&self, other: &Point2) -> Point {
+        self.point.abs_diff(&other.point)
+    }
+}
+
 pub fn part2(input: &str) -> String {
+    let mut y_min = usize::MAX;
+    let mut x_min = usize::MAX;
     let mut y_max = 0;
-    let points = input
+    let mut x_max = 0;
+    let mut points = input
         .lines()
-        .map(|line| {
+        .enumerate()
+        .map(|(i, line)| {
             let (x, y) = line.split_once(',').unwrap();
             let (x, y) = (x.parse().unwrap(), y.parse().unwrap());
             y_max = y_max.max(y);
-            Point { x, y }
+            x_max = x_max.max(x);
+            y_min = y_min.min(y);
+            x_min = x_min.min(x);
+            Point2::new(x, y, Some(i))
         })
         .collect::<Vec<_>>();
 
-    let mut min_x: Option<(usize, Point)> = None;
-    let mut max_x: Option<(usize, Point)> = None;
-    let mut green = vec![(0, 0); y_max + 1];
+    let original_points = points.clone();
+
+    let mut new_points = Vec::new();
+
+    for (i, point) in points.iter().enumerate() {
+        for j in [
+            wrapping(i, true, points.len()),
+            wrapping(i, false, points.len()),
+        ] {
+            let point2 = &points[j];
+            if point <= point2 {
+                continue;
+            }
+            if point.x() == point2.x() {
+                for y in (point.y().min(point2.y()) + 1)..point.y().max(point2.y()) {
+                    new_points.push(Point2::new(point.x(), y, None));
+                }
+            }
+        }
+    }
+    points.extend(new_points);
+
+    let mut greens = vec![Vec::new(); y_max + 1];
+
+    points.sort_unstable_by(|point, point2| {
+        match point.y().cmp(&point2.y()) {
+            std::cmp::Ordering::Equal => {}
+            a => return a,
+        }
+        point.x().cmp(&point2.x())
+    });
     let mut min = None;
-    let mut max = None;
-    for (y, val) in green.iter_mut().enumerate() {
-        let mut y_points = points
-            .iter()
-            .enumerate()
-            .filter(|(_, point)| point.y == y)
-            .map(|(i, point)| (i, *point))
-            .collect::<Vec<_>>();
-        y_points.sort_unstable_by_key(|(_, point)| point.x);
-        if y_points.is_empty() {
-            if let Some(min_x) = min_x
-                && let Some(max_x) = max_x
-            {
-                let mut before_min = wrapping(min_x.0, true, points.len());
-                let mut after_min = wrapping(min_x.0, false, points.len());
-                let mut before_max = wrapping(max_x.0, true, points.len());
-                let mut after_max = wrapping(max_x.0, false, points.len());
-
-                while points[before_min].y <= y && points[after_min].y <= y {
-                    before_min = wrapping(before_min, true, points.len());
-                    after_min = wrapping(after_min, false, points.len());
-                }
-
-                while points[before_max].y <= y && points[after_max].y <= y {
-                    before_max = wrapping(before_max, true, points.len());
-                    after_max = wrapping(after_max, false, points.len());
-                }
-
-                if points[before_min].y > y {
-                    min = Some(points[before_min].x);
-                } else {
-                    min = Some(points[after_min].x);
-                }
-
-                if points[before_max].y > y {
-                    max = Some(points[before_max].x);
-                } else {
-                    max = Some(points[after_max].x);
-                }
-                let min = min.unwrap();
-                let max = max.unwrap();
-                *val = (min, max);
-            }
-            continue;
+    let mut take = false;
+    let mut prev_y = 0;
+    for (j, point) in points.iter().enumerate() {
+        if prev_y != point.y() {
+            min = None;
+            take = false;
         }
-        if let Some(act_min_x) = min_x {
-            if let Some(act_min) = min
-                && let Some(found) = y_points.iter().find(|point| point.1.x == act_min)
-            {
-                min_x = Some(*found);
-            }
-            if y_points[0].1.x <= act_min_x.1.x {
-                min_x = Some(y_points[0]);
+        prev_y = point.y();
+        let prev_take = take;
+        if let Some(i) = point.index {
+            let next_point = &points[wrapping(i, false, points.len())];
+            let next_y_point = points.get(j + 1);
+            if !take {
+                take = true;
+            } else {
+                take = next_point.y() == point.y()
+                    || next_y_point
+                        .is_some_and(|point2| point2.index.is_none() && point2.y() == point.y());
             }
         } else {
-            min_x = Some(y_points[0]);
+            take = !take;
         }
-        if let Some(act_max_x) = max_x {
-            if let Some(act_max) = max
-                && let Some(found) = y_points.iter().find(|point| point.1.x == act_max)
-            {
-                max_x = Some(*found);
-            }
-            if y_points[y_points.len() - 1].1.x >= act_max_x.1.x {
-                max_x = Some(y_points[y_points.len() - 1]);
-            }
-        } else {
-            max_x = Some(y_points[y_points.len() - 1]);
+        if !prev_take && take {
+            min = Some(point.x());
+        } else if prev_take && !take {
+            greens[point.y()].push((min.unwrap(), point.x()));
+            min = None;
         }
-        let act_min_x = min_x.unwrap();
-        let act_max_x = max_x.unwrap();
-        *val = (act_min_x.1.x, act_max_x.1.x);
-        min = None;
-        max = None;
     }
 
     let mut max = 0;
-    let mut other_points = Vec::new();
-    for point1 in points {
-        'innerPoint: for point2 in other_points.iter() {
+    for point1 in original_points.iter() {
+        'innerPoint: for point2 in original_points.iter() {
+            if point1 <= point2 {
+                continue;
+            }
             let sub = point1.abs_diff(point2);
             let area = (sub.x + 1) * (sub.y + 1);
-            let min_x = point1.x.min(point2.x);
-            let max_x = point1.x.max(point2.x);
-            let min_y = point1.y.min(point2.y);
-            let max_y = point1.y.max(point2.y);
-            for (min, max) in green.iter().take(max_y + 1).skip(min_y) {
-                if *min > min_x || *max < max_x {
-                    continue 'innerPoint;
+            let min_x = point1.x().min(point2.x());
+            let max_x = point1.x().max(point2.x());
+            let min_y = point1.y().min(point2.y());
+            let max_y = point1.y().max(point2.y());
+            for bounds in greens.iter().take(max_y + 1).skip(min_y) {
+                for (min, max) in bounds {
+                    if *min > min_x || *max < max_x {
+                        continue 'innerPoint;
+                    }
                 }
             }
             max = max.max(area);
         }
-        other_points.push(point1);
     }
 
     max.to_string().to_owned()
